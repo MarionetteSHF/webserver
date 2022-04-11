26d5f8fe325c6d34e32ec1461a5d2fab9de6e0ff
MarionetteSHF <hanfu.shi2019@gmail.com>
2022-04-11 18:31:50 -0400

    Default Changelist

--- webserver/server.py
+++ webserver/server.py
@@ -16,14 +16,14 @@ Read about it online.
 """
 
 import os
-from sqlalchemy import *
-from sqlalchemy.pool import NullPool
+# from sqlalchemy import *
+# from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
+# tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+# app = Flask(__name__, template_folder=tmpl_dir)
 
-
+app = Flask(__name__)
 
 # XXX: The Database URI should be in the format of: 
 #
@@ -45,47 +45,47 @@ DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
 
 
 #
-# This line creates a database engine that knows how to connect to the URI above
+# # This line creates a database engine that knows how to connect to the URI above
+# #
+# engine = create_engine(DATABASEURI)
 #
-engine = create_engine(DATABASEURI)
-
-
-# Here we create a test table and insert some values in it
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+#
+# # Here we create a test table and insert some values in it
+# engine.execute("""DROP TABLE IF EXISTS test;""")
+# engine.execute("""CREATE TABLE IF NOT EXISTS test (
+#   id serial,
+#   name text
+# );""")
+# engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
 
-@app.before_request
-def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request
-
-  The variable g is globally accessible
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    import traceback; traceback.print_exc()
-    g.conn = None
-
-@app.teardown_request
-def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+# @app.before_request
+# def before_request():
+#   """
+#   This function is run at the beginning of every web request
+#   (every time you enter an address in the web browser).
+#   We use it to setup a database connection that can be used throughout the request
+#
+#   The variable g is globally accessible
+#   """
+#   try:
+#     g.conn = engine.connect()
+#   except:
+#     print("uh oh, problem connecting to database")
+#     import traceback; traceback.print_exc()
+#     g.conn = None
+#
+# @app.teardown_request
+# def teardown_request(exception):
+#   """
+#   At the end of the web request, this makes sure to close the database connection.
+#   If you don't the database could run out of memory!
+#   """
+#   try:
+#     g.conn.close()
+#   except Exception as e:
+#     pass
 
 
 #
@@ -103,6 +103,8 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
+
+  return "hello world"
   """
   request is a special object that Flask provides to access web request information:
 
@@ -114,7 +116,7 @@ def index():
   """
 
   # DEBUG: this is debugging code to see what request looks like
-  print request.args
+  # print(request.args)
 
 
   #
@@ -175,13 +177,12 @@ def another():
 
 
 # Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  print name
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
-  return redirect('/')
+# @app.route('/add', methods=['POST'])
+# def add():
+#   name = request.form['name']
+#   cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
+#   g.conn.execute(text(cmd), name1 = name, name2 = name);
+#   return redirect('/')
 
 
 @app.route('/login')
@@ -212,7 +213,7 @@ if __name__ == "__main__":
     """
 
     HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
+
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
